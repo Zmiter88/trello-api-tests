0e64db8a79ae3af2aa465b1fe82175076f52e239
Patryk Zmitrowicz <zmiter88@gmail.com>
2026-06-24 13:29:22 +0200

    clean classes from old version of tests

--- src/test/java/tests/CreateBoardTest.java
+++ src/test/java/tests/CreateBoardTest.java
@@ -1,195 +1,137 @@
 package tests;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import jdk.jfr.Description;
-import org.apache.commons.lang3.RandomStringUtils;
+import data.RandomDataGenerator;
+import dto.CreateBoardRequest;
+import dto.CreateBoardResponse;
+import dto.ErrorResponse;
+import factory.CreateBoardRequestFactory;
+import io.restassured.response.Response;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import service.BoardsService;
 
-import java.util.List;
-import java.util.Objects;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.UUID;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+
+import static org.hamcrest.Matchers.containsString;
 
 public class CreateBoardTest extends BaseTest {
 
-    @Test
-    public void createBoardWithRequiresFieldsTest() {
-        String boardName = "board " + UUID.randomUUID();
-        String boardId = "69eb592c5a709fd9c3edcf99";
+    BoardsService boardsService = new BoardsService();
 
-        getRequestSpecification()
-                .queryParam("name", boardName)
-                .when()
-                .post("/boards")
-                .then()
-                .log()
-                .all()
-                .statusCode(200)
-                .body("name", equalTo(boardName));
-
-    }
-
-    @Test
-    public void createBoardWithDescription() {
-        String apiKey = System.getenv("TRELLO_KEY");
-        String apiToken = System.getenv("TRELLO_TOKEN");
-        String boardName = "board " + UUID.randomUUID();
-        RestAssured.baseURI = "https://api.trello.com/1";
-        String description = "Opis mojego boarda";
-
-        given()
-                .contentType(ContentType.JSON)
-                .queryParam("name", boardName)
-                .queryParam("key", apiKey)
-                .queryParam("token", apiToken)
-                .queryParam("desc", description)
-                .when()
-                .post("/boards")
-                .then()
-                .log()
-                .all()
-                .statusCode(200)
-                .body("name", equalTo(boardName))
-                .body("desc", equalTo(description));
-    }
-
-    @Test
-    public void createBoardWithoutRequireField() {
-        String apiKey = System.getenv("TRELLO_KEY");
-        String apiToken = System.getenv("TRELLO_TOKEN");
-        RestAssured.baseURI = "https://api.trello.com/1";
-
-        given()
-                .contentType(ContentType.JSON)
-                .queryParam("key", apiKey)
-                .queryParam("token", apiToken)
-                .when()
-                .post("/boards")
-                .then()
-                .log()
-                .all()
-                .statusCode(400)
-                .body("message", equalTo("invalid value for name"));
-
-    }
-
-    @Description("If prefs_background is invalid set default value blue")
-    @Test
-    public void shouldSetDefaultBlueWhenPrefsBackgroundIsInvalid() {
-        String apikey = System.getenv("TRELLO_KEY");
-        String apiToken = System.getenv("TRELLO_TOKEN");
-        String boardName = "board " + UUID.randomUUID();
-        String prefsBackground = "invalid_" + UUID.randomUUID();
-        RestAssured.baseURI = "https://api.trello.com/1";
-
-        getRequestSpecification()
-                .queryParam("key", apikey)
-                .queryParam("token", apiToken)
-                .queryParam("name", boardName)
-                .queryParam("prefs_background", prefsBackground)
-                .when()
-                .post("/boards")
-                .then()
-                .log()
-                .ifValidationFails()
-                .statusCode(200)
-                .body("name", equalTo(boardName))
-                .body("prefs.background", equalTo("blue"));
-
-    }
-
-
-    @Description("If prefs_background is null set default value blue")
-    @Test
-    public void shouldSetDefaultBlueWhenPrefsBackgroundIsNull() {
-        String apiKey = System.getenv("TRELLO_KEY");
-        String apiToken = System.getenv("TRELLO_TOKEN");
-        String boardName = "board " + UUID.randomUUID();
-        String prefsBackground = null;
-        RestAssured.baseURI = "https://api.trello.com/1";
-
-        given()
-                .contentType(ContentType.JSON)
-                .queryParam("key", apiKey)
-                .queryParam("token", apiToken)
-                .queryParam("name", boardName)
-                .queryParam("prefs_background", prefsBackground)
-                .when()
-                .post("/boards")
-                .then()
-                .log()
-                .ifValidationFails()
-                .statusCode(200)
-                .body("name", equalTo(boardName))
-                .body("prefs.background", equalTo("blue"));
-    }
-
-    @Test
-    public void shouldSetValidBackgroundFieldLoopVersion() {
-        String apiKey = System.getenv("TRELLO_KEY");
-        String apiToken = System.getenv("TRELLO_TOKEN");
-        List<String> validColors = List.of("blue", "orange", "green", "red", "purple", "pink", "lime", "sky", "grey");
-        RestAssured.baseURI = "https://api.trello.com/1";
-
-        for (String color : validColors) {
-            String boardName = "board-" + color + "-" + UUID.randomUUID();
-            given()
-                    .contentType(ContentType.JSON)
-                    .queryParam("key", apiKey)
-                    .queryParam("token", apiToken)
-                    .queryParam("name", boardName)
-                    .queryParam("prefs_background", color)
-                    .when()
-                    .post("/boards")
-                    .then()
-                    .log()
-                    .ifValidationFails()
-                    .statusCode(200)
-                    .body("name", equalTo(boardName))
-                    .body("prefs.background", equalTo(color));
-        }
-    }
-
-    @DataProvider(name = "colors")
-    public Object[][] colorsProvider() {
-        return new Object[][] {
-            {"blue"},
-            {"orange"},
-            {"green"},
-            {"red"},
-            {"purple"},
-            {"pink"},
-            {"lime"},
-            {"sky"},
-            {"grey"}
+    @DataProvider(name = "validNames")
+    public Object[][] validNames() {
+        return new Object[][]{
+                {"a"},
+                {"aa"},
+                {"board123"},
         };
     }
 
-    @Test(dataProvider = "colors")
-    public void shouldSetValidBackgroundFieldDataProviderVersion(String color) {
-        String apiKey = System.getenv("TRELLO_KEY");
-        String apiToken = System.getenv("TRELLO_TOKEN");
-        RestAssured.baseURI = "https://api.trello.com/1";
-        String boardName = "board-" + color + "-" + UUID.randomUUID();
+    @Test(dataProvider = "validNames")
+    public void createBoardWithRequiresFieldsMyRefactor(String name) {
 
-            given()
-                    .contentType(ContentType.JSON)
-                    .queryParam("key", apiKey)
-                    .queryParam("token", apiToken)
-                    .queryParam("name", boardName)
-                    .queryParam("prefs_background", color)
-                    .when()
-                    .post("/boards")
-                    .then()
-                    .log()
-                    .ifValidationFails()
-                    .statusCode(200)
-                    .body("name", equalTo(boardName))
-                    .body("prefs.background", equalTo(color));
+        CreateBoardRequest request = CreateBoardRequestFactory.withName(name);
+
+        Response response = boardsService.createBoard(request);
+        assertThat(response.getStatusCode()).isEqualTo(200);
+        CreateBoardResponse createBoardResponse = response.as(CreateBoardResponse.class);
+        assertThat(createBoardResponse.getName()).isEqualTo(name);
+    }
+
+
+    @DataProvider(name = "invalidNames")
+    public Object[][] invalidNames() {
+        return new Object[][] {
+                {""},
+                {null},
+        };
+    }
+
+
+    @Test(dataProvider = "invalidNames")
+    public void createBoardWithInvalidNameShouldFailRefactor(String name) {
+
+        CreateBoardRequest request = CreateBoardRequestFactory.withName(name);
+
+        Response response = boardsService.createBoard(request);
+        assertThat(response.getStatusCode()).isEqualTo(400);
+        ErrorResponse errorResponse = response.as(ErrorResponse.class);
+
+        assertThat(errorResponse.getMessage()).isEqualTo("invalid value for name");
+        assertThat(errorResponse.getError()).isEqualTo("ERROR");
+    }
+
+    @Test
+    public void createBoardWithTooLongNameShouldFailRefactor() {
+
+        String tooLongBoardName = "a".repeat(16385);
+        CreateBoardRequest request = CreateBoardRequestFactory.withName(tooLongBoardName);
+        Response response = boardsService.createBoard(request);
+        assertThat(response.getStatusCode()).isEqualTo(414);
+        assertThat(response.getBody().asString()).contains("414");
+        // albo tak tez mozna
+        response.then().body(containsString("414"));
+    }
+
+    @DataProvider(name = "validColors")
+    public Object[][] validColors() {
+        return new Object[][] {
+                {"blue"},
+                {"orange"},
+                {"green"},
+                {"red"},
+                {"purple"},
+                {"pink"},
+                {"lime"},
+                {"sky"},
+                {"grey"}
+        };
+    }
+
+    @Test(dataProvider = "validColors")
+    public void shouldSetValidBackgroundFieldRefactor(String color) {
+
+        String boardName = "board-" + color + "-" + UUID.randomUUID();
+        CreateBoardRequest request = CreateBoardRequest.builder()
+                .name(boardName)
+                .prefsBackground(color)
+                .build();
+
+        Response response = boardsService.createBoard(request);
+        assertThat(response.getStatusCode()).isEqualTo(200);
+        CreateBoardResponse createBoardResponse = response.as(CreateBoardResponse.class);
+        assertThat(createBoardResponse.getName()).isEqualTo(boardName);
+        assertThat(createBoardResponse.getPrefs().getBackground()).isEqualTo(color);
+    }
+
+    @DataProvider(name = "invalidColors")
+    public Object[][] invalidColors() {
+        return new Object[][] {
+                {""},
+                {" "},
+                {"invalidColor"},
+                {null},
+        };
+    }
+
+    @Test(dataProvider = "invalidColors")
+    public void shouldSetDefaultColorBlueWhenPrefsBackgroundIsInvalidRefactor(String color) {
+
+        String boardName = RandomDataGenerator.boardName();
+        String defaultColor = "blue";
+
+  CreateBoardRequest request = CreateBoardRequest.builder()
+          .name(boardName)
+          .prefsBackground(color)
+          .build();
+
+  Response response = boardsService.createBoard(request);
+  assertThat(response.getStatusCode()).isEqualTo(200);
+  CreateBoardResponse createBoardResponse = response.as(CreateBoardResponse.class);
+  assertThat(createBoardResponse.getName()).isEqualTo(boardName);
+  assertThat(createBoardResponse.getPrefs().getBackground()).isEqualTo(defaultColor);
     }
 }
